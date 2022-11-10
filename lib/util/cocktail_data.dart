20daaf5d8f96fa0ffc3bfbef580c0d7b31835a8e
Ludovico7 <96681298+Ludovico7@users.noreply.github.com>
2022-11-11 00:48:50 +0900

    Add : 칵테일 정보 페이지

---
+++ lib/util/cocktail_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Cocktail{
+  int? id;
+  String? name;
+  String? summary;
+  String? description;
+  String? image;
+
+  Cocktail(this.id, this.name, this.summary, this.description, this.image);
+
+}
+
